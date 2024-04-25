e8f2a9e17ae5f20f136bb93998657416505eeabc
alex00c <alexander.garcia.external@eviden.com>
2024-04-25 08:33:23 +0100

    controller

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
 import express from 'express';
+import porjectsRoutes from './routes/projects.routes.js';
 const app = express();
+
+app.use(porjectsRoutes)
+
 export default app;
---
+++ src/controller/projects.controller.js
@@ -0,0 +1,7 @@
+const getProjects = (req , res) => {
+    res.send('gettin projects');
+}
+
+const createProjects = (req , res) => {
+    res.send('creating projects');
+}
